943b030bdd78eb78fc6375426d20287e8bc7d519
github-main-user <kolya.skripn@mail.ru>
2025-03-19 13:29:05 +0300

    fix formatting

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,7 +1,8 @@
 import json
 from abc import ABC, abstractmethod
+from typing import Any
 
-from .vacancy import Vacancy
+from vacancy import Vacancy
 
 
 class VacancyFileHandler(ABC):
@@ -35,7 +36,7 @@ class VacancyJsonHandler(VacancyFileHandler):
     def __init__(self, file_name: str = "./data/vacancies.json") -> None:
         self.__file_name = file_name
 
-    def _load_vacancies(self):
+    def _load_vacancies(self) -> list | Any:
         """Загружает ванаскии из файла, указанного при инициализации."""
         with open(self.__file_name, "r", encoding="utf-8") as file_obj:
             try:
@@ -45,7 +46,7 @@ class VacancyJsonHandler(VacancyFileHandler):
 
         return vacancies
 
-    def _dump_vacancies(self, vacancies: list[dict]):
+    def _dump_vacancies(self, vacancies: list[dict]) -> None:
         """Выгружает список транзакций в JSON файл, указанный при инициализации."""
         with open(self.__file_name, "w", encoding="utf-8") as file_obj:
             json.dump(vacancies, file_obj, ensure_ascii=False, indent=4)
@@ -70,6 +71,8 @@ class VacancyJsonHandler(VacancyFileHandler):
         for vc in vacancies:
             if pattern.lower() in vc.get("name", "").lower():
                 return Vacancy(**vc)
+
+        return None
 
     def delete_vacancy(self, pattern: str) -> None:
         """Удаляет вакансии, содержащие в названии переданную строку."""
