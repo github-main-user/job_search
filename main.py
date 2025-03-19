79ea78e201c3f55844543ed0fd83efa040b65d81
github-main-user <kolya.skripn@mail.ru>
2025-03-19 13:14:58 +0300

    add file for user interaction

---
+++ main.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+from src.api import HeadHunterAPI
+from src.vacancy import Vacancy
+
+hh_api = HeadHunterAPI()
+
+
+def print_vacancies(vacancies: list[Vacancy]) -> None:
+    """Выводит вакансии в stdout, при этом форматируя вывод."""
+    print(f"\nПо данному запросу найдено {len(vacancies)}, с использованием API HeadHanter", end="\n\n")
+
+    for vacancy in vacancies:
+        print(vacancy, end="\n\n")
+
+
+def user_interaction() -> None:
+    search_query = input("Введите поисковый запрос: ")
+    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    filter_word = input("Введите ключевое слово, для фильтрации по требованиям: ")
+
+    # Получение вакансий
+    vacancies_json = hh_api.get_vacancies(search_query, top_n)
+
+    # Преобразование в список объектов
+    vancancies_object_list = Vacancy.cast_to_object_list(vacancies_json)
+
+    # Фильтрация по ключевому слову в описании
+    filtered_vacancies = [vc for vc in vancancies_object_list if filter_word.lower() in vc.requirement.lower()]
+
+    # Вывод на экран
+    print_vacancies(filtered_vacancies)
+
+
+if __name__ == "__main__":
+    user_interaction()
