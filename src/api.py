aac5064fd5e8db5888c954cf42cfdd1170637fac
github-main-user <kolya.skripn@mail.ru>
2025-03-19 12:19:34 +0300

    add count argument

--- src/api.py
+++ src/api.py
@@ -8,12 +8,12 @@ class APIParser(ABC):
     """Абстрактный класс, описывающий методы работы с API для поиска работы."""
 
     @abstractmethod
-    def _connect(self) -> dict:
+    def _connect(self, params: dict) -> dict:
         """Метод для получения данных с API."""
         pass
 
     @abstractmethod
-    def get_vacancies(self, keyword: str) -> list:
+    def get_vacancies(self, keyword: str, count: int) -> list:
         """Метод для получения списка вакансий, содержащих переданное ключевое слово."""
         pass
 
@@ -24,23 +24,21 @@ class HeadHunterAPI(APIParser):
     def __init__(self) -> None:
         self.__url = "https://api.hh.ru/vacancies"
         self.__headers = {"User-Agent": "HH-User-Agent"}
-        self.__params = {"text": "", "per_page": 10}
 
-    def _connect(self) -> dict | Any:
+    def _connect(self, params: dict) -> dict | Any:
         """Загружает вакансии используя API hh.ru, возвращает полученный JSON."""
-        response = requests.get(self.__url, headers=self.__headers, params=self.__params, timeout=5)
+        response = requests.get(self.__url, headers=self.__headers, params=params, timeout=5)
         response.raise_for_status()
         return response.json()
 
-    def get_vacancies(self, keyword: str) -> list | Any:
+    def get_vacancies(self, keyword: str, count: int = 10) -> list | Any:
         """
         Получает вакансии содержащие переданное ключевое слово, используя API hh.ru,
         возвращает список вакансий в формате JSON.
         """
-        self.__params["text"] = keyword
 
         try:
-            data = self._connect()
+            data = self._connect({"text": keyword, "count": count})
             return data.get("items", [])
         except requests.HTTPError:
             return []
